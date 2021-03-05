2333ca23ba83d2fd1b9f4859258e9063bc7ad3cd
Roberto Casula <casual4free@gmail.com>
2021-03-06 00:10:19 +0100

    fix: navigation to home

--- src/containers/Landing/Landing.tsx
+++ src/containers/Landing/Landing.tsx
@@ -6,7 +6,10 @@ import { useNavigation } from '@react-navigation/native';
 export const Landing = (props: ContainerProps) => {
   const navigation = useNavigation();
   useEffect(() => {
-    setTimeout(() => navigation?.navigate("Home"), 1000)
+    setTimeout(() => navigation?.reset({
+      index: 0,
+      routes: [{ name: 'Home' }],
+    }), 1000)
   }, [navigation])
   return (
     <Text>Loading ...</Text>
