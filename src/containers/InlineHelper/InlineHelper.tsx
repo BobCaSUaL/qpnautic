c168c8d20791bd0b0775ad5500eac4f300f2d0a8
Roberto Casula <casual4free@gmail.com>
2021-03-06 23:45:00 +0100

    fix: back behaviour android for inline helper

--- src/containers/InlineHelper/InlineHelper.tsx
+++ src/containers/InlineHelper/InlineHelper.tsx
@@ -1,6 +1,7 @@
+import { useFocusEffect, useNavigation } from '@react-navigation/native';
 import { Text } from 'native-base';
-import React, { useContext } from 'react';
-import { View } from 'react-native';
+import React, { useCallback, useContext } from 'react';
+import { BackHandler, View } from 'react-native';
 import Touchable from '../../components/Touchable';
 import { TouchableHelperDescriptorI } from '../../components/TouchableHelper';
 import { AppContext } from '../App';
@@ -15,9 +16,25 @@ export const InlineHelper = (props: Props) => {
   const { appDispatch } = useContext(AppContext)
   const helperDescriptor = props.route?.params?.helperDescriptor;
 
+  const dismiss = useCallback(() => appDispatch(appHelperDismiss()), [appDispatch]);
+
+  useFocusEffect(
+    React.useCallback(() => {
+      const onBackPress = () => {
+        dismiss();
+        return true;
+      };
+
+      BackHandler.addEventListener('hardwareBackPress', onBackPress);
+
+      return () =>
+        BackHandler.removeEventListener('hardwareBackPress', onBackPress);
+    }, [])
+  );
+
   return (
     <View style={{ opacity: 0.9, backgroundColor: '#FFF', height: '100%' }}>
-      <Touchable onPress={() => appDispatch(appHelperDismiss())}><Text>CLOSE</Text></Touchable>
+      <Touchable onPress={dismiss}><Text>CLOSE</Text></Touchable>
       <Text>INLINE HELPER</Text>
     </View>
   )
